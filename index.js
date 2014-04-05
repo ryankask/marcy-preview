b02719822467ab9c7e896f6b148b6f3905585b0c
Ryan Kaskel <dev@ryankaskel.com>
2014-04-05 23:00:27 +0100

    Change tense.

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var server = require('./server');
 var port = 3000;
 
 server.start(port, function() {
-  util.log(util.format('Starting Marcy on port %d', port));
+  util.log(util.format('Marcy running on port %d', port));
 });
